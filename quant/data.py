3868959f380c0985cec2a24148c92e6c6a011679
s06a <91134347+s06a@users.noreply.github.com>
2024-02-16 01:04:04 +0330

    [Fix] fixed the adjusted prices to make time series continuous

--- quant/data.py
+++ quant/data.py
@@ -17,8 +17,16 @@ def history(symbols: dict) -> pd.DataFrame:
     df = pd.DataFrame()
 
     if symbols['tsetmc']:
-        tickers = tse.download(symbols=symbols['tsetmc'])
+        tickers = tse.download(symbols=symbols['tsetmc'], adjust=True)
         for ticker in tickers:
-            df.loc[:,ticker] = tickers[ticker]['close']
+            # adjusting the price to make the time series continuous
+            temp = tickers[ticker][['date', 'close', 'adjClose', 'yesterday']]
+            temp.rename(columns={'close': ticker}, inplace=True)
+            temp.set_index('date', inplace=True)
+            temp['ratio'] = (temp['yesterday'].shift(-1) / temp['adjClose']).fillna(1.0)
+            temp['ratio'] = temp.iloc[::-1]['ratio'].cumprod().iloc[::-1]
+            temp[ticker] = temp[ticker].mul(temp['ratio'], axis=0)
+            temp.drop(['ratio', 'adjClose', 'yesterday'], inplace=True, axis=1)
+            df = df._append([df, temp], ignore_index = False)
 
     return df
