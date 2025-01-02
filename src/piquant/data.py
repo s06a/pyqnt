321e511356a117a5c82a9dbd0bdf4f45411c0e42
s06a <91134347+s06a@users.noreply.github.com>
2025-01-02 20:44:47 +0330

    Fix the pandas SettingWithCopyWarning

--- src/piquant/data.py
+++ src/piquant/data.py
@@ -7,8 +7,9 @@ def history(symbols: dict) -> pd.DataFrame:
 
     Parameters:
     ----------
-        - symbols : dictionary
+        - symbols : dict
             provided symbols
+
     Returns:
     ----------
         - df : dataframe
@@ -20,13 +21,13 @@ def history(symbols: dict) -> pd.DataFrame:
         tickers = tse.download(symbols=symbols['tsetmc'], adjust=True)
         for ticker in tickers:
             # adjusting the price to make the time series continuous
-            temp = tickers[ticker][['date', 'close', 'adjClose', 'yesterday']]
+            temp = tickers[ticker][['date', 'close', 'adjClose', 'yesterday']].copy()
             temp.rename(columns={'close': ticker}, inplace=True)
             temp.set_index('date', inplace=True)
             temp['ratio'] = (temp['yesterday'].shift(-1) / temp['adjClose']).fillna(1.0)
             temp['ratio'] = temp.iloc[::-1]['ratio'].cumprod().iloc[::-1]
             temp[ticker] = temp[ticker].mul(temp['ratio'], axis=0)
             temp.drop(['ratio', 'adjClose', 'yesterday'], inplace=True, axis=1)
-            df = df._append([df, temp], ignore_index = False)
+            df = pd.concat([df, temp], ignore_index = False)
 
     return df
